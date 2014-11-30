9fa4ea8b1eea743c8bc0a2184ec6ba6c98d10392
Aimee Matthew-John <aimeebethmj@gmail.com>
2014-11-30 23:17:46 +0000

    Footer and header started

--- wordpress/wp-content/themes/granary-care-aimee-matteo/footer.php
+++ wordpress/wp-content/themes/granary-care-aimee-matteo/footer.php
@@ -1,4 +1,4 @@
-</section>
+<!-- </section>
 <footer class="row">
 	<?php do_action('foundationPress_before_footer'); ?>
 	<?php dynamic_sidebar("footer-widgets"); ?>
@@ -10,6 +10,67 @@
 	</div>
 </div>
 <?php wp_footer(); ?>
-<?php do_action('foundationPress_before_closing_body'); ?>
+<?php do_action('foundationPress_before_closing_body'); ?> -->
+
+	<!-- FOOTER AREA -->
+	<div class="full-width footer-area">
+    <div class="row">
+        <div class="large-4 medium-4 columns">
+          <ul class="footer-nav">
+            <li><a href="#">About Granary Care</a></li>
+            <li><a href="#">Granary Kids</a></li>
+            <li><a href="#">Nanny Agency</a></li>
+            <li><a href="#">Mother and Baby Unit</a></li>
+            <li><a href="#">Social Responsibility</a></li>
+            <li><a href="#">Work For Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="large-5 medium-5 columns">
+          <p class="call-us-today">Call Us Today! <a href="tel:02084233391">020 8423 3391</a></p>
+        </div>
+        <div class="large-3 medium-3 columns social-buttons">
+            <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+            <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+            <a href="#"><i class="fa fa-pinterest fa-2x"></i></a>
+        </div>
+    </div>
+      <div class="row">
+        <div class="large-12 columns">
+          <div>
+            &copy; <?php echo ( bloginfo("name") . " " . date("Y") ); ?>
+          </div>
+        </div>
+      </div>
+  </div>
+
+    
+    <!-- load jQuery -->
+    <script src="<?php echo get_active_theme_directory() ; ?>/js/jquery/dist/jquery.min.js"></script>
+
+    <!-- load Foundation and initialise it -->
+    <script src="<?php echo get_active_theme_directory() ; ?>/js/foundation/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+    
+    <?php if (is_front_page()) : ?>
+
+    	<!-- load Slick and apply it to .slider -->
+	    <script type="text/javascript" src="<?php echo get_active_theme_directory() ; ?>/js/slick/slick.min.js"></script>
+	    <script type="text/javascript">
+	        $(document).ready(function()
+	        {
+	          var slickOptions = 
+	          {
+
+	          }
+
+	          $('.slider').slick(slickOptions);
+	        });
+	    </script>
+
+	<?php endif; ?>
+
 </body>
 </html>
--- wordpress/wp-content/themes/granary-care-aimee-matteo/header.php
+++ wordpress/wp-content/themes/granary-care-aimee-matteo/header.php
@@ -21,7 +21,14 @@
 			echo wp_title( ' | ', 'false', 'right' ); bloginfo( 'name' );
 		} ?></title>
 		
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ; ?>/css/app.css" />
+		<link rel="stylesheet" href="<?php echo get_active_theme_directory(); ?>/css/normalize.css" />
+		<link rel="stylesheet" href="<?php echo get_active_theme_directory(); ?>/css/app.css" />
+		<link rel="stylesheet" href="<?php echo get_active_theme_directory(); ?>/css/font-awesome/css/font-awesome.min.css"> <!-- import icon font  -->
+    	<link rel="stylesheet" href="<?php echo get_active_theme_directory(); ?>/js/slick/slick.css"/> <!-- slick slider -->
+
+    	<!-- our styles rules, after all the other styles! -->
+		<link rel="stylesheet" href="<?php echo get_active_theme_directory(); ?>/css/aimee.css" />
+
 
 		<link rel="icon" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/favicon.ico" type="image/x-icon">
 		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-144x144-precomposed.png">
@@ -30,8 +37,135 @@
 		<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-precomposed.png">
 		
 		<?php wp_head(); ?>
+
 	</head>
+
 	<body <?php body_class(); ?>>
+
+
+	<!-- HEADER AREA -->
+	    <header>
+	        <div class="row">
+	          <div class="large-8 medium-8 columns">
+	            <p class="call-us-today">Call Us Today! <a href="tel:<?php echo ot_get_option( 'telephone_number' ); ?>"><?php echo ot_get_option( 'telephone_number' ); ?></a></p>
+	          </div>
+	          <div class="large-4 medium-4 columns social-buttons">
+	            <a href="<?php echo ot_get_option( 'twitter' ); ?>"><i class="fa fa-twitter fa-2x"></i></a>
+	            <a href="<?php echo ot_get_option( 'facebook' ); ?>"><i class="fa fa-facebook-square fa-2x"></i></a>
+	            <a href="#"><i class="fa fa-pinterest fa-2x"></i></a>
+	          </div>
+	        </div>
+	        
+	        <div class="row">
+	          <div class="large-12 columns">
+	            <h1>
+	            	<a href="<?php echo site_url(); ?>"><?php echo ( bloginfo("name")); ?></a>
+	            </h1>
+	            <h4 class="strapline">
+	            	<?php 
+	            		echo ( bloginfo("description")); 
+	            	?>
+	            </h4>
+	          </div>
+	        </div>
+	    </header>
+
+
+	<!-- NAVIGATION AREA -->
+	  <div class="full-width navigation-area">
+	    <div class="row">
+	        <div class="large-12 columns">
+
+	            <!-- NAV WRAP -->
+	            <nav class="top-bar" data-topbar role="navigation" data-options="is_hover: true">
+	                <ul class="title-area">
+	                    <li class="name"></li>
+	                    <li class="toggle-topbar menu-icon">
+	                      <a href=""><span>Menu</span></a>
+	                    </li>
+	                </ul>
+
+	              <!-- SECTION WRAP -->
+	              <section class="top-bar-section">
+	                
+	                <!-- LEFT NAV SECTION -->
+	                <ul class="left top-level">
+	                    <li class="has-dropdown"><a href="<?php echo site_url(); ?>/about">About</a>
+	                      <ul class="dropdown">
+	                          <li><a href="<?php echo site_url(); ?>/team">Team</a></li>
+	                          <li><a href="<?php echo site_url(); ?>/testimonials">Testimonials</a></li>
+	                      </ul>
+	                    </li>
+
+	                    <li class="has-dropdown"><a href="granarykids.html">Granary Kids</a>
+	                      <ul class="dropdown">
+	                          <li><a href="afterschool-breakfast-clubs.html">After School Clubs and Breakfast Clubs</a></li>
+	                          <li><a href="tippletopples.html">Tipple Topples Creche</a></li>
+	                          <li><a href="#">Active Holiday Camps</a></li>
+	                          <li><a href="#">Working with Granary Kids</a></li>
+	                          <li><a href="#">Locations</a></li>
+	                          <li><a href="#">Book a place</a></li>
+	                          <li><a href="#">Useful info</a></li>
+	                      </ul>
+	                    </li>
+
+	                    <li class="has-dropdown"><a href="nannyagency.html">Nanny Agency</a>
+	                      <ul class="dropdown">
+	                          <li class="has-dropdown"><a href="families.html">Families</a>
+	                            <ul class="dropdown">
+	                                <li><a href="#">Meet Our Nannies</a></li>
+	                                <li><a href="typesofcarers.html">Types of Carers</a></li>
+	                                <li><a href="#">Useful Info</a></li>
+	                                <li><a href="#">Agency Services</a></li>
+	                                <li><a href="#">Register</a></li>
+	                            </ul>
+	                          </li>  
+	                          <li class="has-dropdown"><a href="#">Nannies</a>
+	                            <ul class="dropdown">
+	                                <li><a href="#">Job Search</a></li>
+	                                <li><a href="#">Register</a></li>
+	                                <li><a href="#">Agency Services</a></li>
+	                                <li><a href="#">Useful Info</a></li>
+	                            </ul>
+	                          </li>  
+	                      </ul>
+	                    </li>
+
+	                    <li class="has-dropdown"><a href="#">Mother and Baby Unit</a>
+	                      <ul class="dropdown">
+	                          <li><a href="#">Mother and Baby Services</a></li>
+	                          <li><a href="#">Referral Process and Fees</a></li>
+	                          <li><a href="#">Documents</a></li>
+	                          <li><a href="#">Facilities</a></li>
+	                      </ul>
+	                    </li>
+
+	                    <li><a href="#">Social Responsibility</a></li>
+
+	                    <li class="has-dropdown"><a href="#">Work for Us</a>
+	                      <ul class="dropdown">
+	                          <li><a href="#">Granary Kids</a></li>
+	                          <li><a href="#">Granary Nanny Agency</a></li>
+	                          <li><a href="#">Mother and Baby Unit</a></li>
+	                      </ul>
+	                    </li>
+
+	                    <li><a href="#">Contact Us</a></li>
+	                      
+	                </ul>
+
+	                <!-- RIGHT NAV SECTION -->
+	                <!-- <ul class="right"></ul> -->
+	              
+	              </section>
+	            </nav>
+	      </div>
+	    </div>
+	  </div>
+    
+
+
+<!-- 
 	<?php do_action('foundationPress_after_body'); ?>
 	
 	<div class="off-canvas-wrap" data-offcanvas>
@@ -55,4 +189,6 @@
 	<?php get_template_part('parts/top-bar'); ?>
 
 <section class="container" role="document">
-	<?php do_action('foundationPress_after_header'); ?>
+	<?php do_action('foundationPress_after_header'); ?> 
+
+-->
