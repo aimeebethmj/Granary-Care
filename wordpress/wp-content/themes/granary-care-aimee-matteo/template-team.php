4ca84bd7a463de306069f7af2466b1ce04a837f2
Aimee Matthew-John <aimeebethmj@gmail.com>
2014-12-04 23:11:49 +0000

    Added banner to team template + small tweaks

--- wordpress/wp-content/themes/granary-care-aimee-matteo/template-team.php
+++ wordpress/wp-content/themes/granary-care-aimee-matteo/template-team.php
@@ -4,7 +4,24 @@ Template Name: Team Page
 */
 get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+
+    $categories = get_the_category(); // returns all categories assigned to a page/post as an Array
+    $category = $categories[0]; // let's grab the first category in the Array (the element at index 0)
+    $slug = $category->slug; // kind of self-explanatory (the slug property inside the category Object)
+
+?>
+
+
+<!-- BANNER STATEMENT -->
+
+<div class="full-width content-area banner-statement <?php echo $slug; ?>" >
+    <div class="row">
+        <div class="large-12 medium-12 small-12 columns">
+            <h2><?php echo get_field(banner_message);?></h2>
+        </div>
+    </div>
+</div>
 
 
 <!-- STAFF PROFILES GRANARY CARE -->
@@ -43,7 +60,7 @@ get_header(); ?>
         <div class="large-3 medium-3 columns">
           <img class="staff-profile-images" src="<?php echo $image['url']; ?>"/>      
         </div>
-        <div class="large-9 medium-9 columns">
+        <div class="large-9 medium-9 columns staffinfo">
           <h4><?php echo $staffname; ?></h4>
           <h5><?php echo $jobtitle; ?></h5>
           <p><?php echo $jobroleinfo; ?></p>
@@ -53,6 +70,7 @@ get_header(); ?>
         </div>
       </div>
     </div>
+    <hr>
 
   <?php endwhile; ?>
 
