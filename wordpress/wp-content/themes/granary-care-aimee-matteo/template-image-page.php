86f5b335da9fbb8849eab81e997a9cf26339ab5c
Aimee Matthew-John <aimeebethmj@gmail.com>
2014-12-12 18:23:11 +0000

    New page for facilities

---
+++ wordpress/wp-content/themes/granary-care-aimee-matteo/template-image-page.php
@@ -0,0 +1,117 @@
+<?php
+/*
+Template Name: Image page
+*/
+get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+
+ 	$categories = get_the_category(); // returns all categories assigned to a page/post as an Array
+    $category = $categories[0]; // let's grab the first category in the Array (the element at index 0)
+    $slug = $category->slug; // kind of self-explanatory (the slug property inside the category Object)
+
+
+?>
+
+<!-- BANNER STATEMENT -->
+
+<div class="full-width content-area banner-statement <?php echo $slug; ?>" >
+    <div class="row">
+        <div class="large-12 medium-12 small-12 columns">
+            <h2><?php echo get_field('banner_message');?></h2>
+        </div>
+    </div>
+</div>
+
+
+<!-- SUMMARY -->
+
+<div class="full-width content-area page-summary1">
+  <div class="row">
+    <div class="large-12 medium-12 columns">
+    <h2><?php echo get_the_title(); ?></h2>
+         <?php the_content();?>
+    </div>
+  </div>
+</div>
+
+
+<!-- FACILITIES PANELS -->
+<div class="full-width content-area page-summary3 facilities">
+  <div class="row">
+    <h2><?php echo get_field('image_section_title'); ?></h2>
+
+<?php if( have_rows('image_section') ): ?>
+
+  <?php while( have_rows('image_section') ): the_row();
+
+    // Vars
+    $image = get_sub_field('panel_image');
+    $panelTitle = get_sub_field('panel_title');
+    $blurb = get_sub_field('blurb');
+
+  ?>
+
+    <div class="large-6 medium-6 columns activity-business-panel">
+          <div class="large-button-image">
+            <img src="<?php echo $image['url']; ?>">
+            
+          </div>
+          <div class="business-panel-description">
+            <h3><?php echo $panelTitle; ?></h3>
+            <?php echo $blurb; ?>
+          </div>
+        </div>
+
+ <?php endwhile; ?>
+
+<?php endif; ?>
+
+  </div>
+</div>
+
+<?php 
+
+$secondSection = get_field('second_section');
+$secondButtonLink = get_field('section_section_button');
+$secondButtonLabel = get_field('second_section_button_label');
+
+if( !empty( $secondSection ) ): ?>
+<div class="full-width content-area page-summary1">
+  <div class="row">
+    <div class="large-12 medium-12 columns">
+      <?php echo $secondSection;?>
+    </div>
+  </div>
+</div>
+<?php endif; ?>
+
+<!-- ACTION BUTTONS -->
+<div class="full-width content-area action-area">
+  <div class="row">
+    <div class="large-12 medium-12 columns buttons-container">
+      <a class="large success round button" href="<?php echo get_field('action_button_1'); ?>"><?php echo get_field('action_button_1_label'); ?></a><a class="large success round button" href="<?php get_field('action_button_2'); ?>"><?php echo get_field('action_button_2_label'); ?></a>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php endwhile; ?>
+<!-- post navigation -->
+<?php else: ?>
+<!-- no posts found -->
+<?php endif; ?>
+
+<?php get_footer(); ?>
