33a2c045e3989821e025859803bcfc55e55abb3b
Matteo Menapace <hello@baddeo.com>
2015-05-17 20:40:37 +0100

    Improved consoleLog function

--- wordpress/wp-content/themes/granary-care-aimee-matteo/functions.php
+++ wordpress/wp-content/themes/granary-care-aimee-matteo/functions.php
@@ -66,12 +66,12 @@ function getCategorySlug()
 }
 
 // a handy function to "console.log" PHP data
-function consoleLog($data) {
-	if (is_array($data) || is_object($data)) {
-		echo("<script>console.log(".json_encode($data).");</script>");
-	} else {
-		echo("<script>console.log(".$data.");</script>");
-	}
+function consoleLog($data) 
+{
+	if (is_array($data) || is_object($data)) $data = json_encode($data);
+	else if (is_string($data)) $data = '"' . $data . '"';
+	
+	echo('<script>console.log(' . $data . ');</script>');
 }
 
 
