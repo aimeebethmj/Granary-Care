06a884d50bfd00d5d365663f700bd0f0255c891e
Aimee Matthew-John <aimeebethmj@gmail.com>
2015-09-08 15:17:25 +0100

    404 page

--- wordpress/wp-content/themes/granary-care-aimee-matteo/404.php
+++ wordpress/wp-content/themes/granary-care-aimee-matteo/404.php
@@ -1,26 +1,27 @@
 <?php get_header(); ?>
 
 <div class="row">
-	<div class="small-12 large-8 columns" role="main">
+	<div class="small-12 large-10 small-centered columns" role="main">
 
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+		
 			<header>
-				<h1 class="entry-title"><?php _e('File Not Found', 'FoundationPress'); ?></h1>
+				<h1 class="entry-title">Oops!</h1>
 			</header>
-			<div class="entry-content">
-				<div class="error">
-					<p class="bottom"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'FoundationPress'); ?></p>
-				</div>
-				<p><?php _e('Please try the following:', 'FoundationPress'); ?></p>
-				<ul>
-					<li><?php _e('Check your spelling', 'FoundationPress'); ?></li>
-					<li><?php printf(__('Return to the <a href="%s">home page</a>', 'FoundationPress'), home_url()); ?></li>
-					<li><?php _e('Click the <a href="javascript:history.back()">Back</a> button', 'FoundationPress'); ?></li>
-				</ul>
+			<div class="row">
+				 <div class="large-10 medium-12 small-centered columns">
+	                <div class="image-container mainimage">
+	                  <img src="http://granarycare.com/wp-content/uploads/playdough-404.jpg">
+	                </div>
+	                <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p> 
+					<p>Try to find the page through the main navigation bar.</p>
+					<div class="heart-mark-yellow" style="margin-bottom: 2rem;"></div>
+  				</div>
+
 			</div>
-		</article>
+
+		
 
 	</div>
-	<?php get_sidebar(); ?>
+	
 </div>
 <?php get_footer(); ?>
