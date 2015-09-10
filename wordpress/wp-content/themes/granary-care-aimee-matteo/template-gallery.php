aa87f39418eb34c8a72e3f05aa7ac8cb11369bbf
Aimee Matthew-John <aimeebethmj@gmail.com>
2015-09-10 17:06:28 +0100

    Gallery template

---
+++ wordpress/wp-content/themes/granary-care-aimee-matteo/template-gallery.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Gallery
+*/
+get_header(); 
+
+// summon the GLOBAL variable (assigned in header.php)
+global $categorySlug;
+
+?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<!-- CONTENT AREA -->
+<div class="content">
+
+<!-- BANNER STATEMENT -->
+
+<div class="full-width content-area banner-statement <?php echo $categorySlug; ?>" >
+    <div class="row">
+        <div class="large-10 medium-12 small-centered columns">
+            <!-- :before -->
+            <h2><?php echo get_field('banner_message');?></h2>
+            <!-- :after -->
+        </div>
+    </div>
+</div>
+
+
+
+    <div class="row">
+
+      <div class="large-10 medium-12 small-centered columns">
+
+        <?php the_content();?>
+
+        <div id="instafeed"></div>
+
+      
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
